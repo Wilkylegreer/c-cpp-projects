5d5f187da070ec303062b0a3ff6f2630f472a82a
Wilkylegreer <wilkylegreer@gmail.com>
2023-11-28 21:56:42 -0700

    New files

---
+++ cpp_sandbox/exercise_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int x = (10 - 5);
+
+    return 0;
+}
--- cpp_sandbox/main.cpp
+++ cpp_sandbox/main.cpp
@@ -2,6 +2,34 @@
 
 int main()
 {
+
+    int h = 10;
+    int i = ++h;
+    int j = h++;
+
+    std::cout << "I : " << i << std::endl;
+    std::cout << "I X: " << h << std::endl;
+    std::cout << "J : " << j << std::endl;
+    std::cout << "J X : " << h << std::endl;
+
+    double x = 10;
+    int y = 3;
+    double z = x / y;
+
+    std::cout << "X / Y: " << z << std::endl;
+
+    const double pi = 3.14;
+
+    int a = 1;
+    int b = 2;
+    int bucket = a;
+
+    a = b;
+    b = bucket;
+
+    std::cout << "A: " << a << std::endl;
+    std::cout << "B: " << b << std::endl;
+
     int age = 25;
     double height = 5.9;
     char grade = 'A';
