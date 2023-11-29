a569e1e7f27e66cc7ab740fa6df06f44b727c8f4
Wilkylegreer <wilkylegreer@gmail.com>
2023-11-28 19:10:07 -0700

    Initial commit with existing files

---
+++ cpp_sandbox/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int age = 25;
+    double height = 5.9;
+    char grade = 'A';
+    std::string name = "Kyle";
+
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Height: " << height << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+
+    return 0;
+}
